d6dc93d37c0fa939316ea659a369ec1b7c543789
Solara <145303732+solbinjung@users.noreply.github.com>
2024-04-25 20:12:15 +1000

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	
+	int n, x;
+	int a[10000];
+
+	scanf("%d %d", &n, &x);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	for (int j = 0; j < n; j++)
+		if (x > a[j])
+			printf("%d ", a[j]);
+	
+	return 0;
+}

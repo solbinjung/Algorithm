8c0344dbfadb14bce34f8583214a78d8247c6f54
Solara <145303732+solbinjung@users.noreply.github.com>
2026-02-04 15:06:54 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> arr) {
+    for(int i=0; i<arr.size(); i++) {
+        for(int j=0; j<arr[i].size(); j++){
+            if(i != j) {
+                if(arr[i][j]==arr[j][i]) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }  
+        }
+    }
+}
